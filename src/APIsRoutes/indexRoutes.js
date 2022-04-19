453862b734c6986a38df01f947071e203ac02609
jcarloshg <carlosj12336@gmail.com>
2022-04-19 16:47:06 -0500

    CREARE && IMPLEMENT file [utilsRouter]

--- src/APIsRoutes/indexRoutes.js
+++ src/APIsRoutes/indexRoutes.js
@@ -1,25 +1,10 @@
 const connection = require("../Frameworks/database/mysql/connection");
+const utilsRouter = require("../utils/utils.routes");
 const examRouter = require("./APIExamen/APIExamen.routes");
 
 const indexRoutes = (app) => {
 
-    app.get('/', (request, response) => {
-        response.status(200).json({ mensaje: "Esta API funciona como back-end del proyecto SaludJusta" });
-    });
-
-    app.get('/check_mysql', (request, response) => {
-        // response.status(200).json({ mensaje: "Esta API funciona como back-end del proyecto SaludJusta" });
-        connection.connect(function (err) {
-            if (err) {
-                console.error('Database connection failed: ' + err.stack);
-                response.status(200).json(err);
-                return;
-            }
-
-            console.log('Connected to database.');
-            response.status(200).json(err);
-        });
-    })
+    app.use('/utils', utilsRouter);
 
     app.use('/api_exam', examRouter);
 
---
+++ src/utils/utils.routes.js
@@ -0,0 +1,33 @@
+const { Router } = require("express");
+const connection = require("../Frameworks/database/mysql/connection");
+
+const utilsRouter = Router();
+
+
+utilsRouter.get('/', (request, response) =>
+    response
+        .status(200)
+        .json({ mensaje: "Esta API funciona como back-end del proyecto SaludJusta" }))
+
+
+
+utilsRouter.get('/check_coneciont_mysql', (request, response) => {
+    connection.connect((err) => {
+        if (err) {
+            console.error('Database connection failed: ' + err.stack);
+            response
+                .status(200)
+                .json(err);
+            return;
+        }
+
+        console.log('Connected to database.');
+        response
+            .status(200)
+            .json({ message: "Conecction sucess" });
+    });
+
+    connection.end();
+});
+
+module.exports = utilsRouter;
