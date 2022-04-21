bbf84caaaa78425d8e552b0e022dde4ca646a9bf
jcarloshg <carlosj12336@gmail.com>
2022-04-21 18:48:26 -0500

    CREATE endpoint [requestAppointmentsDay]

--- src/APIs/APIAppointment/CRUD/requestAppointmentsDay.js
+++ src/APIs/APIAppointment/CRUD/requestAppointmentsDay.js
@@ -1,7 +1,12 @@
+const { DATE } = require("mysql/lib/protocol/constants/types");
 
 
 
 const requestAppointmentsDay = async (connection, data) => {
+
+    const today = new Date();
+
+    console.log(`[today] -> `, today);
 
     return await "requestAppointmentsDay is Ok";
 
