a49ad7d90b849156fe166ce8eddc31f78ffbe2fc
jcarloshg <carlosj12336@gmail.com>
2022-04-22 12:22:33 -0500

    ADD comments to function  [requestAppointmentsDay]

--- src/APIs/APIAppointment/CRUD/requestAppointmentsDay.js
+++ src/APIs/APIAppointment/CRUD/requestAppointmentsDay.js
@@ -1,6 +1,6 @@
 
 //============================================================
-// his name -> marcarCitaComoEncurso
+// his name -> solicitaCitasDelDia()
 // data ->
 //      [date] his format is YYYY-MM-DD -> 2022-04-21
 //============================================================
