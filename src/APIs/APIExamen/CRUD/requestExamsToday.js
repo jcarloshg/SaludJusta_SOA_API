e08ed640f528d941a397c43e8bac42cf42b884a2
jcarloshg <carlosj12336@gmail.com>
2022-04-30 11:35:04 -0500

    MODIFY endpoint [requestExamsToday]

--- src/APIs/APIExamen/CRUD/requestExamsToday.js
+++ src/APIs/APIExamen/CRUD/requestExamsToday.js
@@ -9,11 +9,11 @@
 
 const Appointment = require("../../../entities/Appointment");
 const Exam = require("../../../entities/Exam");
+const ExamCatalogItem = require("../../../entities/ExamCatalogItem");
+const User = require("../../../entities/User");
 const conectionQuery = require("../../../Frameworks/database/mysql/conection.query");
 
 const requestExamsToday = async (connection) => {
-
-    console.log(`[requestExamsToday] -> `,);
 
     const today = new Date('2022-04-21'); // TODO change to today
     const todayString = today.toISOString().split('T')[0];
@@ -21,15 +21,28 @@ const requestExamsToday = async (connection) => {
     try {
 
         const query =
-            "SELECT * FROM Exam INNER JOIN Appointment ON Appointment.FK_Exam = Exam.idExam " +
-            `WHERE status = 'EN ESPERA' AND date = '${todayString}'` +
+            "SELECT * FROM Exam " +
+            "INNER JOIN ExamCatalog ON Exam.FK_ExamCatalog = ExamCatalog.idExamCatalog " +
+            "INNER JOIN Appointment ON Appointment.FK_Exam = Exam.idExam " +
+            "INNER JOIN User ON User.idUser = Appointment.FK_UserClient " +
+            `WHERE status = 'EN CURSO' AND date = '${todayString}'` +
             "ORDER BY time ASC;";
 
         const infoExams = await conectionQuery(connection, query);
 
         return infoExams.map(infoExam => {
-            const auxExam = new Exam(infoExam);
-            return new Appointment({ ...infoExam, exam: auxExam });
+
+
+            const examCatalogItem = new ExamCatalogItem(infoExam);
+            const auxExam = new Exam({ ...infoExam, examCatalogItem: examCatalogItem });
+            const appointment = new Appointment({ ...infoExam, exam: auxExam });
+            const user = new User({ ...infoExam, appointments: [appointment] });
+            // console.log(`[examCatalogItem] -> `, examCatalogItem);
+            // console.log(`[auxExam] -> `, auxExam);
+            // console.log(`[appointment] -> `, appointment);
+            // console.log(`[user] -> `, user);
+
+            return user;
         });
 
     } catch (error) {
--- src/entities/Exam.js
+++ src/entities/Exam.js
@@ -1,14 +1,19 @@
 
+const ExamCatalogItem = require("./ExamCatalogItem");
 class Exam {
 
     constructor({
         idExam = null,
         results = null,
         FK_ExamCatalog = null,
+        // reference
+        examCatalogItem = new ExamCatalogItem({})
     }) {
         this.idExam = idExam;
         this.results = results;
         this.FK_ExamCatalog = FK_ExamCatalog;
+        // reference
+        this.examCatalogItem = examCatalogItem;
     }
 }
 
